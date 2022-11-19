246a0080a5d4a51a91dc00ea0200c93af2fbaa28
Bahree <bahree36@outlook.com>
2022-11-20 06:33:23 +0700

    added hero section

---
+++ client/src/components/HeroSection/HeroSection.jsx
@@ -0,0 +1,26 @@
+import LinkButton from "../LinkButton/LinkButton";
+import ContentWrapper from "../ContentWrapper/ContentWrapper";
+import HeroBackground from "../HeroBackground/HeroBackground";
+
+function HeroSection() {
+    return (
+        <section className='heroSection flex' style={{ position: "relative" }}>
+            <ContentWrapper>
+                <div className='heroSection__container flex'>
+                    <h1 className='heroSection__tagline ff-sans fw-black'>
+                        Our fruits is organic, locally <br /> sourced and picked by hand <br /> to make sure you only
+                        get
+                        <br />
+                        the best quality.
+                    </h1>
+
+                    <LinkButton to={"/"} name={"Get me some fruits"} />
+                </div>
+            </ContentWrapper>
+
+            <HeroBackground />
+        </section>
+    );
+}
+
+export default HeroSection;
