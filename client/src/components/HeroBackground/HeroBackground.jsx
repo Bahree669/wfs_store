08e838b83e6a0bf475505ce4412e7a53de89aaaf
Bahree <bahree36@outlook.com>
2022-11-20 06:32:28 +0700

    added hero background

---
+++ client/src/components/HeroBackground/HeroBackground.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const HeroBackground = () => {
+    return (
+        <div className='heroBackground'>
+            <div className='heroBackground__container'>
+                <div className='heroBackground__image'></div>
+            </div>
+        </div>
+    );
+};
+
+export default HeroBackground;
