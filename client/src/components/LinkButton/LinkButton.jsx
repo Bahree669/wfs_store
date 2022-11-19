68c8b63143ab211053ecacb81b026c296ccfb391
Bahree <bahree36@outlook.com>
2022-11-20 06:34:00 +0700

    added link buttons

--- client/src/components/LinkButton/LinkButton.jsx
+++ client/src/components/LinkButton/LinkButton.jsx
@@ -2,8 +2,9 @@ import { Link } from "react-router-dom";
 
 function LinkButton({ to, name, variant }) {
     return (
-        <Link to={to} className={`linkButton linkButton--${variant} ff-sans fw-sm fs-n focus__ring`}>
-            {name}
+        <Link to={to} className={`linkButton linkButton--${variant} ff-sans fw-sm fs-sm focus__ring`}>
+            <span className='linkButton__name'>{name}</span>
+            <span className='linkButton__wiper'>{name}</span>
         </Link>
     );
 }
