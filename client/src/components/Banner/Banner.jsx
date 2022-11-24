c1bee0c68d6b150aae027a20fa3ec8ac99e79cfb
Bahree <bahree36@outlook.com>
2022-11-24 18:06:00 +0700

    added banner component

---
+++ client/src/components/Banner/Banner.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Banner = () => {
+    return (
+        <div className='banner'>
+            <div className='banner__container'>
+                <div className='banner__image'></div>
+            </div>
+        </div>
+    );
+};
+
+export default Banner;
