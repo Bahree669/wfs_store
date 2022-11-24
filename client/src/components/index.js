3632a66fd3ef387fe17655a01326526f39911fe1
Bahree <bahree36@outlook.com>
2022-11-24 18:10:50 +0700

    exported reusable components

---
+++ client/src/components/index.js
@@ -0,0 +1,6 @@
+export { default as Banner } from "./Banner/Banner";
+export { default as Footer } from "./Footer/Footer";
+export { default as Header } from "./Header/Header";
+export { default as HomeProduct } from "./HomeProduct/HomeProduct";
+export { default as HeroSection } from "./HeroSection/HeroSection";
+export { default as MissionSection } from "./MissionSection/MissionSection";
