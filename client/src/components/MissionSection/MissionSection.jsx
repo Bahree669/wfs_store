0bdbe88414a16cbe5c4f8f18225d272f7e990e8e
Bahree <bahree36@outlook.com>
2022-11-24 18:06:52 +0700

    refactore the getWidth fn

--- client/src/components/MissionSection/MissionSection.jsx
+++ client/src/components/MissionSection/MissionSection.jsx
@@ -8,31 +8,31 @@ import VideoModal from "../VideoModal/VideoModal";
 const MissionSection = () => {
     const [containerWidth, setContainerWidth] = useState(90);
 
+    const getWidth = () => {
+        const outputStart = 90;
+        const outputEnd = 100;
+
+        const inputStart = 0;
+        const inputEnd = 100;
+
+        const y = scrollY / 5;
+        const input = y <= 100 ? y : 100;
+
+        const output = outputStart + ((outputEnd - outputStart) / (inputEnd - inputStart)) * (input - inputStart);
+
+        setContainerWidth(output);
+    };
+
     useEffect(() => {
-        const getWidth = () => {
-            const outputStart = 90;
-            const outputEnd = 100;
-
-            const inputStart = 0;
-            const inputEnd = 100;
-
-            const input = scrollY / 5 <= 100 ? scrollY / 5 : 100;
-
-            const output = outputStart + ((outputEnd - outputStart) / (inputEnd - inputStart)) * (input - inputStart);
-
-            setContainerWidth(output);
-        };
-
-        window.addEventListener("scroll", getWidth);
-
+        window.addEventListener("scroll", getWidth, false);
         return () => {
             window.removeEventListener("scroll", getWidth);
         };
-    }, [containerWidth]);
+    }, []);
 
     return (
         <div className='gradient__black'>
-            <section className='missionSection' style={{ "--ms-width": `${containerWidth}%` }}>
+            <section className='missionSection' style={{ "--c-width": `${containerWidth}%` }}>
                 <ContentWrapper>
                     <div className='missionSection__content'>
                         <GridContainer variant={"reverse"}>
